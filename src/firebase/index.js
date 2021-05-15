a0c9fb914f9d10ac8ee0657bab9e69d1f983b333
Kyle Leary (KlutchDEV) <klutchdevelopment@gmail.com>
2021-05-15 04:44:31 -0400

    Update index.js

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,17 +1,22 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/analytics";
+import "firebase/firestore";
 
-firebase.initializeApp({
-  apiKey: "AIzaSyCviibf8x5UEBQrqyKwBf46784JOLtDxLs",
-  authDomain: "klutch-todo.firebaseapp.com",
-  projectId: "klutch-todo",
-  storageBucket: "klutch-todo.appspot.com",
-  messagingSenderId: "73597702061",
-  appId: "1:73597702061:web:bb458c85fedccf1e701b24",
-  measurementId: "G-L0GWFMWK5T",
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: "AIzaSyCviibf8x5UEBQrqyKwBf46784JOLtDxLs",
+    authDomain: "klutch-todo.firebaseapp.com",
+    projectId: "klutch-todo",
+    storageBucket: "klutch-todo.appspot.com",
+    messagingSenderId: "73597702061",
+    appId: "1:73597702061:web:bb458c85fedccf1e701b24",
+    measurementId: "G-L0GWFMWK5T",
+  });
+}
+
 firebase.analytics();
+
 export const auth = firebase.auth();
 export const storage = firebase.storage();
 export const firestore = firebase.firestore();
